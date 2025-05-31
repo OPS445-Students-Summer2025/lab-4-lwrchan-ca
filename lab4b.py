67de184529debe80147471583e2feb04c0b43850
Leung Wai Rene Chan <lwrchan@myseneca.ca>
2025-05-31 15:26:47 -0400

    revised lab4b.py to change output of set() to list()

--- lab4b.py
+++ lab4b.py
@@ -5,15 +5,15 @@
 
 def join_lists(l1, l2):
     # join_lists will return a list that contains every value from both l1 and l2
-    return set(l1)| set(l2)   
+    return list(set(l1)| set(l2))   
 
 def match_lists(l1, l2):
     # match_lists will return a list that contains all values found in both l1 and l2
-    return set(l1) & set(l2)
+    return list(set(l1) & set(l2))
 
 def diff_lists(l1, l2):
     # diff_lists will return a list that contains all different values, which are not shared between the lists
-    return set(l1) ^ set(l2)
+    return list(set(l1) ^ set(l2))
 
 if __name__ == '__main__':
     list1 = list(range(1,10))
